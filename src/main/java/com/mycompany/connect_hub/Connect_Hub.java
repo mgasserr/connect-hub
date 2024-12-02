80861eff83a09c9247f9a0e6e63b215b97698fd9
Mohamed <mohammed.shamsia25@gmail.com>
2024-12-02 02:14:48 +0200

    Main

---
+++ src/main/java/com/mycompany/connect_hub/Connect_Hub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.connect_hub;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Connect_Hub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
