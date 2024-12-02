c596b466f92638de798e4969e680364a864b07be
yassinmady <yassinmady2003@yahoo.com>
2024-12-02 03:40:58 +0300

    validation
    all validations

--- src/main/java/Backend/Validations.java
+++ src/main/java/Backend/Validations.java
@@ -9,5 +9,31 @@ package Backend;
  * @author hp
  */
 public class Validations {
-    
+
+    public static boolean isValidName(String Name) {
+        for (char c : Name.toCharArray()) {
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidPhone(String phoneNumber) {
+        if (phoneNumber != null && phoneNumber.length() == 11 && phoneNumber.matches("[0-9]+")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidEmail(String Email) {
+        if (Email == null || !Email.contains("@") || !Email.contains(".")) {
+            return false;
+        }
+        String[] parts = Email.split("@");
+        if (parts.length != 2 || parts[0].isEmpty() || parts[1].isEmpty() || !parts[1].contains(".")) {
+            return false;
+        }
+        return true;
+    }
 }
