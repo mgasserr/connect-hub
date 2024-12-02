237238ea56557113f4e12472a445f46f46b0fe73
trtl88 <moh.gasser.2004.88@gmail.com>
2024-12-02 03:31:22 +0200

    variable rename

--- src/main/java/Backend/Database/SaveAccount.java
+++ src/main/java/Backend/Database/SaveAccount.java
@@ -9,15 +9,15 @@ import org.json.JSONObject;
 public class SaveAccount {
 
     public static void saveAccount(Account account) {
-            JSONObject j = new JSONObject();
-            j.put("accountid", account.getUserId());
-            j.put("email", account.getEmail());
-            j.put("dob", account.getDob());
-            j.put("password", account.getPassword());
+            JSONObject js = new JSONObject();
+            js.put("accountid", account.getUserId());
+            js.put("email", account.getEmail());
+            js.put("dob", account.getDob());
+            js.put("password", account.getPassword());
         
         try {
             FileWriter file = new FileWriter("users.json");
-            file.write(j.toString(4));
+            file.write(js.toString(4));
             file.close();
         } catch (IOException e) {
             System.out.println("Error");
