005b1a8d50b39ada4c93df3b81d3665f6f3df82d
Mohamed <mohammed.shamsia25@gmail.com>
2024-12-06 14:43:20 +0200

    Modification for add content

--- src/main/java/Frontend/General/Startup.java
+++ src/main/java/Frontend/General/Startup.java
@@ -177,7 +177,7 @@ public class Startup extends javax.swing.JFrame {
         // TODO add your handling code here:
         String username = usernameText.getText();
         String password = passwordText.getText();
-        Account acc = new Log().login(username, password);
+        Account acc = Register.getInstance().signIn(username, password);
         if (acc == null) {
             loginerrorLabel.setText("Wrong username/password");
         } else {
