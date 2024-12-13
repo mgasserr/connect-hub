639cc6bb866a5a7102becaefe43963f64fd02d8b
trtl88 <moh.gasser.2004.88@gmail.com>
2024-12-13 03:52:30 +0200

    creating general search window

--- src/main/java/frontend/general/Home.java
+++ src/main/java/frontend/general/Home.java
@@ -69,6 +69,7 @@ public class Home extends javax.swing.JFrame {
         View = new javax.swing.JButton();
         Groups = new javax.swing.JButton();
         notisToggle = new javax.swing.JToggleButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Home");
@@ -148,6 +149,8 @@ public class Home extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("Search");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -172,20 +175,20 @@ public class Home extends javax.swing.JFrame {
                         .addComponent(notisToggle, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(Settings)
-                        .addGap(18, 18, 18)
+                        .addGap(17, 17, 17)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(View)
+                        .addGap(328, 328, 328)
                         .addComponent(LogOut)))
                 .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(View)
-                .addGap(416, 416, 416))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(6, 6, 6)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(LogOut)
                     .addComponent(Settings)
                     .addComponent(NewContent)
                     .addComponent(Friends)
@@ -193,11 +196,14 @@ public class Home extends javax.swing.JFrame {
                     .addComponent(Home)
                     .addComponent(jLabel1)
                     .addComponent(Groups)
-                    .addComponent(notisToggle))
+                    .addComponent(notisToggle)
+                    .addComponent(jButton1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 332, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(7, 7, 7)
-                .addComponent(View)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(View)
+                    .addComponent(LogOut))
                 .addContainerGap())
         );
 
@@ -348,6 +354,7 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JButton Profile;
     private javax.swing.JButton Settings;
     private javax.swing.JButton View;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JList<String> newsFeed;
